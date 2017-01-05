315872e7534883ff77317c7211a86eac4de643e5
fortran <qnev89@gmail.com>
2017-01-05 22:48:39 +0200

    add java source file with example

---
+++ SimpleTest.java
@@ -0,0 +1,11 @@
+public class SimpleTest {
+
+    public static void customMain() {   
+         System.out.println("C++ is the best programming language ever!!!");
+         int number1 = 10;
+         int number2= 15;
+         int result = number1+number2;
+         
+         System.out.println("\n The SUM is:" + result);
+     }
+}
